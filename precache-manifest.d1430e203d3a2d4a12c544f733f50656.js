f1a5e9e9bc867aa2a8ccb58d1565e4c52d55373c
hari <haripallikere93@gmail.com>
2020-01-21 14:16:14 +0530

    Updates

---
+++ precache-manifest.d1430e203d3a2d4a12c544f733f50656.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "693f68721d801e907895000f05222efa",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "d06dd9f163712b2769b6",
+    "url": "/monster-rolodex/static/css/main.1df9e2ad.chunk.css"
+  },
+  {
+    "revision": "f5c59ff7d2bcec7481a7",
+    "url": "/monster-rolodex/static/js/2.040d0fe2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.040d0fe2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d06dd9f163712b2769b6",
+    "url": "/monster-rolodex/static/js/main.ae97ef97.chunk.js"
+  },
+  {
+    "revision": "cd391451e66ac7af83df",
+    "url": "/monster-rolodex/static/js/runtime-main.1356d2e6.js"
+  }
+]);
